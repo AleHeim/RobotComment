894fc2d798ac63190b8464108ff6a5d1badbf686
AleHeim <70062777+AleHeim@users.noreply.github.com>
2024-04-13 18:12:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,17 +18,12 @@ for video_path in video_paths:
     print('\n video_path: ', video_path)
     video_file_path = os.path.join(VIDEO_DIR_PATH, video_path)
     cap = cv2.VideoCapture(video_file_path)
-    print('1')
     ret = True
 
-    while(True):
+    while ret:
         ret, frame = cap.read()
-        print('2')
         results = model.track(frame, persist=True)
-        print('3')
         frame_ = results[0].plot()
-        print('4')
         cv2.imshow('frame', frame_)
-        print('5')
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
