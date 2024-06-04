c855b9648382593582ca69d6a9bca3683c2b9c4e
AleHeim <70062777+AleHeim@users.noreply.github.com>
2024-06-04 03:18:15 +0300

    using match case instead of if, added arguments

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import supervision as sv
 import cv2
 import os
 import time as t
+import argparse
+
 
 # Constants
 VIDEO_EXTENSIONS = ["mov", "mp4", "mkv"]
@@ -11,15 +13,31 @@ HOME = "./"  # Adjust this to the appropriate home directory
 main_model = YOLO('./runs/detect/train3/weights/best.pt')
 
 
-# Config
-check_bad_detections = True
+'''arguments parser'''
+parser = argparse.ArgumentParser()
+parser.add_argument("mode", type=str, choices=['track','train','split','label'], help="Track: Track object.\nTrain: Train model.\nSplit: Split video by frames.\nLabel: auto-labeling tool.\n")
+parser.add_argument("-cbd", "--check_bad_detections", type=int, nargs="?", default=False, help="usage: -cbd <expected amount of objects>")
+parser.add_argument("-dir", "--directory", action="store_true")
+parser.add_argument("-i", "--input", type=str,default=f"{HOME}videos", help="input device, directory or http/https address")
+
+# Добавить аргументы
+
+args = parser.parse_args()
+
+'''Для аргумента --cbd проверяет, введено ли число'''
+if args.check_bad_detections == None:
+    print(f"error: correct usage: -cbd <expected amount of objects>\nfor example: -cbd 2")
+    quit()
+
+print(args)
 
 
 def create_directories():
     """Create necessary directories if they don't exist."""
-    for directory in ['./videos', './images']:
+    for directory in [f'{HOME}videos', f'{HOME}images']:
         if not os.path.exists(directory):
             os.makedirs(directory)
+    return 0
 
 def split_video():
     """Split videos into frames."""
@@ -35,6 +53,7 @@ def split_video():
         with sv.ImageSink(IMAGE_DIR_PATH, image_name_pattern=image_name_pattern) as sink:
             for image in sv.get_video_frames_generator(str(video_path), stride=FRAME_STRIDE):
                 sink.save_image(image=image)
+    return 0
 
 def track_video_dir(model, source):
     """Track objects in a video."""
@@ -48,9 +67,11 @@ def track_video_dir(model, source):
     for video_path in video_paths:
         video_file_path = os.path.join(".", video_path)
         track_video(model, video_file_path)
+    return 0
 
 def track_video(model, device_id):
     """Track objects from a video"""
+    timeout = None
     frame_number = 0
     average_fps = 0.0
 
@@ -67,30 +88,37 @@ def track_video(model, device_id):
             ret, frame = cap.read()
             if not ret:
                 break
-            start = t.time()
+            start = t.time() #FPS COUNTER
             results = model.predict(frame, verbose=False)#, persist=True, tracker = 'bytetrack.yaml', verbose=False)
             # frame_ = results[0].plot()
-            # cv2.imshow('frame', frame_)
+            # cv2.imshow('frame', frame_)     ### ADDITIONAL LINES FOR model.track()
             if cv2.waitKey(30) & 0xFF == ord('q'):
                 break
             track_ids = results[0].boxes
             try:
-                # track_ids = results[0].boxes.xywh.cpu()
                 track_ids = results[0].boxes.cls
             except AttributeError:
                 track_ids = []
-            # print(f'Len: {len(track_ids)}\n{track_ids}')
-            if (len(track_ids) != 2 and check_bad_detections):
+            if (len(track_ids) != args.check_bad_detections and args.check_bad_detections is not False):
                 cv2.imwrite(f'./datasets/bad_detections/{device_id}/frame_{frame_count:05d}.jpg', frame)
                 print(f'Bad detection saved in ./datasets/bad_detections/{device_id} as frame_{frame_count:05d}.jpg')
                 frame_count+=1
-                if(len(track_ids) == 1):
+            if(len(track_ids) == 1 and args.check_bad_detections is False):
+                if(timeout is not None):
+                    if(t.time()-timeout>=5):
+                        if(input("winner? write y/n")=="y"):
+                            print(f'winner: {track_ids[0]}')
+                            break
+                        else:
+                            timeout = None
+                else:
+                    timeout=t.time()
                     # print(f'winner: {track_ids[0]}')
-                    print(f'winner: {track_ids[0]}')
-                    break
+            else:
+                timeout = None
             
             
-            end = t.time()
+            end = t.time() #FPS COUNTER
             frame_number = frame_number + 1
             fps = 1.0/(end-start)
             average_fps += fps
@@ -99,11 +127,13 @@ def track_video(model, device_id):
 
     cap.release()
     cv2.destroyAllWindows()
+    return 0
 
 def train_model():
     """Train the YOLO model."""
     model = YOLO('yolov8s.yaml')  # Adjust the configuration file as needed
     results = model.train(data="config.yaml", epochs=110)  # Adjust epochs as needed
+    return 0
 
 def list_devices():
     """List available video devices."""
@@ -114,25 +144,45 @@ def list_devices():
             devices.append(i)
             cap.release()
     print("Available video devices:", devices)
+    return 0
 
-def autolabel(model, image_dir, save_dir):
+def autolabel(model, images_dir, save_dir):
     image_paths = sv.list_files_with_extensions(
-    directory=image_dir,
+    directory=images_dir,
     extensions=IMAGE_EXTENSIONS)
-    print('umages: ', image_paths)
+    print('images: ', image_paths)
     for image in image_paths:
         results = main_model.predict(image)
         image_name = os.path.basename(image)
 
         results[0].save_txt(f'{save_dir}/{image_name[:image_name.rindex('.')]}.txt')
+    return 0
 
 
 if __name__ == '__main__':
+    match args.mode:
+        case "track":
+            if(args.directory):
+                track_video_dir(main_model, args.input)
+            else:
+                track_video(main_model, args.input)
+        case "train":
+            train_model()
+        case "split":
+            split_video()
+        case "label":
+            images_dir = './datasets/one/images'
+            save_dir = './datasets/one/labels'
+            autolabel(main_model, images_dir, save_dir)
+
+
+
+'''
     print('1: Tracking\n2: Training\n3: Split video by frames\nChoose mode and write a number:')
     mode = input()
 
     if mode == '1':
-        print('1: Tracking from device\n2: Tracking from video directory\n3: Auto label\nChoose input option:')
+        print('1: Tracking from device\n2: Tracking from directory\n3: Auto label\nChoose input option:')
         input_option = input()
 
         if input_option == '1':
@@ -145,12 +195,13 @@ if __name__ == '__main__':
             track_video_dir(main_model, video_dir)
 
         elif input_option == '3':
-            image_dir = './datasets/one/images'
-            save_dir = 'datasets/one/labels'
-            autolabel(main_model, image_dir, save_dir)
+            images_dir = './datasets/one/images'
+            save_dir = './datasets/one/labels'
+            autolabel(main_model, images_dir, save_dir)
 
     elif mode == '2':
         train_model()
 
     elif mode == '3':
         split_video()
+'''
